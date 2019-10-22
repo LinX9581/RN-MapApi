039df56fd0cbcd35b2b9b68e14d1882376b9631d
linx9581 <linx9581@gmail.com>
2019-10-22 10:09:09 +0800

    add tab bottom

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
-import { Platform, Text, View, StyleSheet, Dimensions,Image } from "react-native";
+import { Platform,Text,View,StyleSheet,Dimensions,Image,TouchableOpacity } from "react-native";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
 import Constants from "expo-constants";
 import * as Permissions from "expo-permissions";
 import Polyline from "@mapbox/polyline";
+import Dialog from "react-native-dialog";
+import * as ImagePicker from "expo-image-picker";
 
 const locations = require("./locations.json");
-const { width, height } = Dimensions.get('screen')
+const { width, height } = Dimensions.get("screen");
 
 export default class App extends Component {
   state = {
     latitude: null,
     longitude: null,
     locations: locations,
+    dialogVisible: false,
   };
 
   async componentDidMount() {
@@ -64,6 +67,7 @@ export default class App extends Component {
     }
   };
 
+  // 取得起始和目的經緯度 用google direction api 取得路徑資訊
   async getDirections(startLoc, desLoc) {
     try {
       const resp = await fetch(
@@ -71,6 +75,9 @@ export default class App extends Component {
       );
       // console.log(resp)
       const respJson = await resp.json();
+
+      //需要取得距離資訊才用
+      
       // console.log(respJson)
       // const response = respJson.routes[0];
       // const distanceTime = response.legs[0];
@@ -93,10 +100,14 @@ export default class App extends Component {
     }
   }
 
-  async onTargetPress () {
-    console.log("testtetetset")
+  async onTargetPress() {
+    console.log("testtetetset");
   }
 
+
+  //地圖標記 start
+
+  //標記被按下
   onMarkerPress = location => () => {
     const {
       coords: { latitude, longitude },
@@ -126,7 +137,13 @@ export default class App extends Component {
               coordinate={{ latitude, longitude }}
               onPress={this.onMarkerPress(location)}
             >
-              <Image source={{uri:"https://p1.hiclipart.com/preview/180/349/86/super-mario-icons-super-mario-star-illustration-thumbnail.jpg"}} style={{height: 35, width:35 }} />
+              <Image
+                source={{
+                  uri:
+                    "https://p1.hiclipart.com/preview/180/349/86/super-mario-icons-super-mario-star-illustration-thumbnail.jpg",
+                }}
+                style={{ height: 35, width: 35 }}
+              />
             </Marker>
           );
         })}
@@ -134,37 +151,102 @@ export default class App extends Component {
     );
   };
 
+  //地圖標記 end
+
+
+  //dialog start 
+  showDialog = () => {
+    this.setState({ dialogVisible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ dialogVisible: false });
+  };
+
+  handleDelete = () => {
+    // The user has pressed the "Delete" button, so here you can do your own logic.
+    // ...Your logic
+    this.setState({ dialogVisible: false });
+  };
+
+  //dialog end
+
+  _takePhoto = async () => {
+    const { status: cameraPerm } = await Permissions.askAsync(
+      Permissions.CAMERA
+    );
+
+    const { status: cameraRollPerm } = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+
+    // only if user allows permission to camera AND camera roll
+    if (cameraPerm === "granted" && cameraRollPerm === "granted") {
+      let pickerResult = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [9, 9],
+      });
+      console.log(pickerResult)
+      // this.setState({ image: pickerResult.uri });
+      // ImgRecgnize(pickerResult.uri);
+    }
+  };
+
+
   render() {
     const { latitude, longitude, coords, destination } = this.state;
 
     if (latitude) {
       return (
-        <MapView
-          showsUserLocation
-          style={styles.mapStyle}
-          initialRegion={{
-            latitude,
-            longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          {this.renderMarkers()}
-
-          <Text> 取得定位中 </Text>
-          <Image
-            source={{ uri: destination && destination.image_url }}
-            style={{
-              flex: 1,
-              width: width * 0.95,    //螢幕寬
-              alignSelf: "center",
-              height: height * 0.20,  //螢幕高
-              position: "absolute",
-              bottom: height * 0.05,
+        <View>
+          <MapView
+            showsUserLocation
+            style={styles.mapStyle}
+            initialRegion={{
+              latitude,
+              longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
             }}
-            onPress={this.onTargetPress()}
-          />
-        </MapView>
+          >
+            {this.renderMarkers()}
+
+            <Text> 取得定位中 </Text>
+
+            <Image
+              source={{ uri: destination && destination.image_url }}
+              style={{
+                flex: 1,
+                width: width * 0.95, //螢幕寬
+                alignSelf: "center",
+                height: height * 0.2, //螢幕高
+                position: "absolute",
+                bottom: height * 0.05,
+              }}
+              onPress={this.onTargetPress()}
+            />
+          </MapView>
+
+            
+          <View style={naviStyle.bottmContainer}>
+            
+            <TouchableOpacity style={[naviStyle.button, { backgroundColor: "#53423D" }]}>
+              <Text onPress={this._takePhoto} style={naviStyle.buttonText}>拍照解成就 </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[naviStyle.button, { backgroundColor: "#A58987" }]}>
+              <Text style={naviStyle.buttonText}>達題解成就</Text>
+            </TouchableOpacity>
+          </View>
+
+          <Dialog.Container visible={this.state.dialogVisible}>
+            <Dialog.Title>Account delete</Dialog.Title>
+            <Dialog.Description>
+              Do you want to delete this account? You cannot undo this action.
+            </Dialog.Description>
+            <Dialog.Button label="Cancel" onPress={this.handleCancel} />
+            <Dialog.Button label="Delete" onPress={this.handleDelete} />
+          </Dialog.Container>
+        </View>
       );
     }
     return (
@@ -191,5 +273,44 @@ const styles = StyleSheet.create({
   mapStyle: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+});
+
+const naviStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bottmContainer: {
+    height: 60,
+    flexDirection: "row",
+  },
+  background: {
+    height: 800,
+    width: 600,
+    position: "absolute",
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 30,
+    color: "#fff",
+    fontWeight: "bold",
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+  desc: {
+    fontSize: 20,
+    color: "#fff",
+    backgroundColor: "rgba(0,0,0,0)",
+    textAlign: "center",
   },
 });
